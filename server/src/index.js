fce77bcf8ee6c73ab7846c4ebffeda0e77e37092
Maksym Moskalchuk <maksym.moskalchuk@gmail.com>
2022-07-02 11:16:49 +0300

    add session support

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import session from "express-session";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    credentials: true,
+  })
+);
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+  })
+);
 
 app.listen(PORT, () =>
-  console.log(`The server is up and running on PORT ${PORT}`));
+  console.log(`The server is up and running on PORT ${PORT}`)
+);
