bd00545899160bdf87e95440b3247557027b7112
Maksym Moskalchuk <maksym.moskalchuk@gmail.com>
2022-07-02 11:45:56 +0300

    set up passport

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,30 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import session from "express-session";
+import passport from "passport";
+import { Strategy } from "passport-google-oauth20";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+passport.use(
+  new Strategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    (accessToken, refreshToken, profile, cb) =>
+      cb({ id: profile.id, name: profile.displayName })
+  )
+);
+
+passport.serializeUser((user, cb) =>
+  cb(null, { id: user.id, name: user.name })
+);
+passport.deserializeUser((user, cb) => cb(null, user));
 
 app.use(
   cors({
@@ -19,6 +38,19 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
   })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => res.redirect("/")
 );
 
 app.listen(PORT, () =>
