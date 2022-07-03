416f1c4f4b58ba2d5f47cfce58ca86bd366212fa
Maksym Moskalchuk <maksym.moskalchuk@gmail.com>
2022-07-04 00:19:07 +0300

    add time formatting

--- client/src/AuthenticatedApp.jsx
+++ client/src/AuthenticatedApp.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
 import Container from "@mui/material/Container";
 import YouTube from "react-youtube";
+import { format } from "date-fns";
 import styles from "./AuthenticatedApp.module.scss";
 
 const AuthenticatedApp = () => {
   const [player, setPlayer] = useState(null);
+
+  const formatTime = (time) => format(time * 1000, "mm:ss");
+
+  const createNote = (text) => {
+    const note = { text, time: player.getCurrentTime() };
+  };
 
   return (
     <Container maxWidth="md" sx={{ py: 3 }}>
