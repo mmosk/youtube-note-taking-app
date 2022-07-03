3dc6b6ea10c74a58e357bdbf96eb8b349612d380
Maksym Moskalchuk <maksym.moskalchuk@gmail.com>
2022-07-03 23:49:01 +0300

    add youtube player component

--- client/src/AuthenticatedApp.jsx
+++ client/src/AuthenticatedApp.jsx
@@ -1,7 +1,22 @@
-import { BrowserRouter } from "react-router-dom";
+import { useState } from "react";
+import Container from "@mui/material/Container";
+import YouTube from "react-youtube";
+import styles from "./AuthenticatedApp.module.scss";
 
 const AuthenticatedApp = () => {
-  return <BrowserRouter></BrowserRouter>;
+  const [player, setPlayer] = useState(null);
+
+  return (
+    <Container maxWidth="md" sx={{ py: 3 }}>
+      <div className={styles.youtubePlayerWrapper}>
+        <YouTube
+          className={styles.youtubePlayer}
+          videoId="2g811Eo7K8U"
+          onReady={(event) => setPlayer(event.target)}
+        />
+      </div>
+    </Container>
+  );
 };
 
 export default AuthenticatedApp;
